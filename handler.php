7bd944393beb85ab64c7d35731707bcbd790b336
MATT CHROMEBOOK <mbaseball1@gmail.com>
2022-07-05 19:41:19 -0500

    logs

--- handler.php
+++ handler.php
@@ -7,16 +7,19 @@ Tested working with PHP5.4 and above (including PHP 7 )
 
  */
 require_once './vendor/autoload.php';
-
+error_log('1');
 use FormGuide\Handlx\FormHandler;
 
+error_log('2');
 
 $pp = new FormHandler(); 
+error_log('3');
 
 $validator = $pp->getValidator();
 $validator->fields(['name','email'])->areRequired()->maxLength(50);
 $validator->field('email')->isEmail();
 $validator->field('comments')->maxLength(6000);
+error_log('4');
 
 
 
@@ -28,5 +31,6 @@ $headers = "From: $email_from \r\n";
 
   $headers .= "Reply-To: $visitor_email \r\n";
 mail('mbaseball1@gmail.com',field['name'],field('comments'),$headers);
+error_log('5');
 
 echo $pp->process($_POST);
